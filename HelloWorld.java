bad1f6922af8a2a588d74e4ea60b9e11e872e1ec
EC2 Default User <ec2-user@ip-172-31-37-99.us-east-2.compute.internal>
2020-10-18 03:45:54 +0000

    friday line added

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
     System.out.println("Hello devops guys");
     System.out.println("Hello devops guys");
     System.out.println("Gowthu Good vibes");
+    System.out.println("Happy weekend");
   }
 }
