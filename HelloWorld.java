4f66cdfd41cefb8e2ace5a9a18bf711ccd28d522
EC2 Default User <ec2-user@ip-172-31-37-99.us-east-2.compute.internal>
2020-10-18 04:35:12 +0000

    Happy me

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
     System.out.println("Hello devops guys");
     System.out.println("Gowthu Good vibes");
     System.out.println("Happy weekend");
-    System.out.println("weekend party lets ghave a drink");
+    System.out.println("weekend party lets have a drink");
+    System.out.println("Happy me");
   }
 }
