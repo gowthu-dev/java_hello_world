4ea214abc7a76c90fdaed0642fa87f7fedd25137
EC2 Default User <ec2-user@ip-172-31-37-99.us-east-2.compute.internal>
2020-10-18 03:53:18 +0000

    Pary started

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
     System.out.println("Hello devops guys");
     System.out.println("Gowthu Good vibes");
     System.out.println("Happy weekend");
+    System.out.println("weekend party lets ghave a drink");
   }
 }
